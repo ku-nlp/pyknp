8e622f23954249839ff81453553d12b0b383496b
John Richardson <trickytoforget@gmail.com>
2015-04-09 13:08:27 +0900

    Simple for JUMAN.

---
+++ juman/simple.py
@@ -0,0 +1,19 @@
+#-*- encoding: utf-8 -*-
+from juman import Juman
+import unittest
+
+JUMAN = Juman()
+
+def juman(input_str):
+    return JUMAN.analysis(input_str)
+
+class SimpleTest(unittest.TestCase):
+    def test(self):
+        test_str = "この文を解析してください。"
+        result = juman(test_str)
+        self.assertEqual(len(result.mrph), 7)
+        self.assertEqual(''.join(x.midasi for x in result.mrph), test_str)
+        self.assertGreaterEqual(len(result.spec().split("\n")), 7)
+
+if __name__ == '__main__':
+    unittest.main()
