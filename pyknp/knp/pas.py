dba663102ae779b53847dbdf1d7e4d53fd5e5892
Hajime Morita <pnnc205j@gmail.com>
2017-02-23 23:59:11 +0900

    項の主辞’代表表記取り出しを修正

--- pyknp/knp/pas.py
+++ pyknp/knp/pas.py
@@ -5,6 +5,8 @@ from __future__ import absolute_import
 import collections
 import six
 
+import sys
+
 class Argument(object):
     def __init__(self, sid, tid, rep, flag=None, sdist=None):
         assert isinstance(tid, int)
@@ -46,6 +48,7 @@ class Pas(object):
     def is_valid(self):
         return self.valid
    
+    # TODO: 代表表記系列か 正規化代表表記 の両系統に対応する必要がある
     def get_predicate_repname(self):
         """
         基本句の代表表記を返す
@@ -81,31 +84,41 @@ class Pas(object):
         for arg in self.arguments[case]:
             tid = arg.tid
             rep = self.tag_list[tid].repname
-            output.append(ArgRepname("+".join(rep),tid)) 
+            output.append(ArgRepname("+".join(rep), tid)) 
         return output
     
     def get_long_arguments(self, case):
         """
-        項の主辞’代表表記相当を与える．
+        項の主辞’代表表記相当の表記と項が含まれる基本句idリストのタプルを返す．
+         
+        項の主辞’代表表記相当を与える．=> 項の主辞'代表表記があれば取り出す
         主辞が一文字漢字であり，直前が名詞の場合は，その直前の基本句を連結した表記を返す．
         格に対して項は複数存在しうるので，戻り値は配列を渡す．
         
         Usage:
         self.get_long_arguments(ガ) 
-        > [ "研究/けんきゅう+者/しゃ" ]
+        > [ ("研究/けんきゅう+者/しゃ", [1,2]) ]
         """
         output = []
+            
         for arg in self.arguments[case]:
             tids = [arg.tid]
             long_rep = []
-            
             if(u"一文字漢字" in self.tag_list[arg.tid].features 
                     and arg.tid > 0 
-                    and self.tag_list[arg.tid-1].mrph_list()[-1].hinsi == "名詞"):
+                    and self.tag_list[arg.tid-1].mrph_list()[-1].hinsi in ["名詞","接頭辞"]):
                 tids.insert(0, arg.tid-1)
-            for tid in tids:
-                long_rep.append(self.tag_list[tid].repname)
-            output.append(ArgRepname("+".join(long_rep),tids)) 
+
+            long_argument = ""
+            if(u"主辞’代表表記" in self.tag_list[arg.tid].features and
+                self.tag_list[arg.tid].features[u"主辞’代表表記"] is not None and
+                len(self.tag_list[arg.tid].features[u"主辞’代表表記"]) > 0 ):
+                long_argument = self.tag_list[arg.tid].features[u"主辞’代表表記"]
+            else: 
+                for tid in tids:
+                    long_rep.append(self.tag_list[tid].repname)
+                long_argument = "+".join(long_rep)
+            output.append(ArgRepname(long_argument,tids)) 
         return output
      
     def get_orig_result(self):
