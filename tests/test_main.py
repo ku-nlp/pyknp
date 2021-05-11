98c0df82a6367c928b60d317e2c50407c8b40a46
nobu-g <ueda-n127@hotmail.co.jp>
2021-05-12 00:50:36 +0900

    refactor

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,25 @@
-import pyknp
-import time
-import pytest
-import threading
 import concurrent.futures
+import time
+
+import pytest
+
+import pyknp
 
 
 @pytest.fixture
 def knp():
     return pyknp.KNP()
 
+
 def task(knp):
     text = "今日はいい天気だった"
     blist = knp.parse(text)
     assert text == "".join(b.midasi for b in blist)
 
+
 def test_knp(knp):
     task(knp)
+
 
 def test_knp_multithread(knp):
     with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
